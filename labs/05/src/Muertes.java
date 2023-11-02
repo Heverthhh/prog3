ace58b96678e676cc6f6b2169a2f9bb7a26de7e2
Heverthhh <141367248+Heverthhh@users.noreply.github.com>
2023-11-01 22:33:27 -0500

    Add files via upload

---
+++ labs/05/src/Muertes.java
@@ -0,0 +1,12 @@
+public class Muertes {
+    String dia;
+    String genero;
+    int edad;
+
+    public Muertes(String dia, String genero, int edad) {
+        this.dia = dia;
+        this.genero = genero;
+        this.edad = edad;
+    }
+
+}
