44205346f357bd03cd2099790c1257b4f9cf822a
Heverthhh <141367248+Heverthhh@users.noreply.github.com>
2023-11-16 13:33:58 -0500

    Add files via upload

---
+++ labs/02/03/InterfazCar.java
@@ -0,0 +1,12 @@
+package com.mycompany.interfazcar;
+
+/**
+ *
+ * @author Jacobo
+ */
+public class InterfazCar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
